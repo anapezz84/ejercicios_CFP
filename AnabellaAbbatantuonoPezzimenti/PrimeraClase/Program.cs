faa798c5be0d7c0f1790ebd9fdb838e111339087
anapezz84 <anabella.pezz.abba@gmail.com>
2024-03-18 21:26:18 -0300

    Primer ejercicio

---
+++ AnabellaAbbatantuonoPezzimenti/PrimeraClase/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PrimeraClase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            string edadTexto;
+            int edadNumerica;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su apellido: ");
+            apellido = Console.ReadLine();
+
+            Console.Write("Ingrese su edad: ");
+            edadTexto = Console.ReadLine();
+
+            edadNumerica = int.Parse(edadTexto);
+
+            Console.WriteLine("Bienvenido " + nombre + " " + apellido + " ud tiene: " + edadNumerica + " años ");
+
+            Console.WriteLine($"Bienvenido"); 
+
+
+        }
+    }
+}
